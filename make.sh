c922f9bcec01698d2a327ed2da9e0abfab332527
yuukimmm <120291279+yuukimmm@users.noreply.github.com>
2023-08-05 15:36:36 +0900

    Update make.sh

--- make.sh
+++ make.sh
@@ -4,7 +4,7 @@ set -eu
 
 DEST=build
 REPO=https://codeberg.org/mofusky/homepage.git
-COMMIT=e553a963d0c6a2cc9e323caf8090c62e4b50ffbd
+COMMIT=73785b4613902d597944f7a8f87f7edf6bd58252
 
 if [ -d "$DEST" ]; then
   rm -rf "$DEST"
